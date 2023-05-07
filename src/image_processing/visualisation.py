44e942f38588aebd381ef6ef341370df6ad30bbe
Hubert <biros@student.agh.edu.pl>
2023-05-07 14:23:58 +0200

    Convert data to binary files

---
+++ src/image_processing/visualisation.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def show_data(x, n=5, height=64, width=64, title=""):
+    plt.figure(figsize=(10,3))
+    for i in range(n):
+        ax = plt.subplot(2,n,i+1)
+        plt.imshow(x[i])
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.suptitle(title, fontsize=16)
+    plt.show()
