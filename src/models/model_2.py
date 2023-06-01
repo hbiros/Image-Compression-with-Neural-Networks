fffa9c72823cc03fd372ed8a79f69726a3a2e82e
hbiros <poczta@hbiros.pl>
2023-06-01 02:39:24 +0200

    Add new model with compression ratio of 6

---
+++ src/models/model_2.py
@@ -0,0 +1,27 @@
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose
+from keras.models import Model
+import keras
+
+from pupieapp_metric.pu_pieapp_metric import PUPieAppMetric
+
+pool_factor = (2,2) 
+
+input_layer = Input(shape=(64, 64, 3), name="input_layer")
+
+conv_1 = Conv2D(16, (3,3), activation='relu', padding='same', name='conv_1')(input_layer)
+pool_1 = MaxPooling2D(pool_factor, name='pool_1')(conv_1)
+conv_2 = Conv2D(8, (3,3), activation='relu', padding='same', name='conv_2')(pool_1)
+
+code_layer = MaxPooling2D(pool_factor, name='code_layer')(conv_2)
+
+unconv_layer_1 = Conv2DTranspose(8, (3,3), activation='relu', padding='same', name='unconv_layer_1')(code_layer)
+upsamp_layer_2 = UpSampling2D(pool_factor, name='upsamp_layer_2')(unconv_layer_1)
+unconv_layer_2 = Conv2DTranspose(16, (3,3), activation='relu', padding='same', name='unconv_layer_2')(upsamp_layer_2)
+upsamp_layer_3 = UpSampling2D(pool_factor, name='upsamp_layer_3')(unconv_layer_2)
+output_layer = Conv2D(3, (3,3), padding='same', activation='sigmoid', name='output_layer')(upsamp_layer_3)
+
+model = Model(input_layer, output_layer)
+
+opt = keras.optimizers.Adam(learning_rate=0.01)
+model.compile(optimizer=opt, loss='mse', run_eagerly=True, metrics=[PUPieAppMetric()])
+encoder = Model(inputs=model.input, outputs=model.get_layer('code_layer').output)
